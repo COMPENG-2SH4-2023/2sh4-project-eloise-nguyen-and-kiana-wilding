5f88c7e28c14d5285fbf153bb5d31dcc4bcc55a7
wildingk1 <144181997+wildingk1@users.noreply.github.com>
2023-11-30 20:05:08 -0500

    11/30 8:02pm

--- Project.cpp
+++ Project.cpp
@@ -3,16 +3,18 @@
 #include "objPos.h"
 #include "GameMechs.h"
 #include "Player.h"
-<<<<<<< HEAD
 #include "Food.h"
 
-=======
->>>>>>> 9326ca0dedc180c54a65b7726c964f6cd51bf2ab
+
+
 
 using namespace std;
 
 
-<<<<<<< HEAD
+
+
+
+
 
 
 #define DELAY_CONST 100000
@@ -20,26 +22,26 @@ using namespace std;
 
 
 
-=======
-#define DELAY_CONST 100000
 
 
->>>>>>> 9326ca0dedc180c54a65b7726c964f6cd51bf2ab
+
+
 //Global Variables
 GameMechs *myGM;
 Player *myPlayer;
 objPos myPos;
-<<<<<<< HEAD
 Food *myFood;
 
 
 
 
+
+
+
+
 bool exitFlag;
-=======
 
 
->>>>>>> 9326ca0dedc180c54a65b7726c964f6cd51bf2ab
 
 
 void Initialize(void);
@@ -54,7 +56,18 @@ void CleanUp(void);
 
 
 
-<<<<<<< HEAD
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -65,10 +78,14 @@ int main(void)
 {
 
 
+
+
    Initialize();
 
 
-   while(myGM->getExitFlagStatus() == false) 
+
+
+   while(myGM->getExitFlagStatus() == false)
    {
        GetInput();
        RunLogic();
@@ -79,69 +96,66 @@ int main(void)
 
 
 
+
+
+
+
    CleanUp();
 
 
 
-=======
-int main(void)
-{
 
 
-    Initialize();
 
-
-    while(myGM->getExitFlagStatus() == false)  
-    {
-        GetInput();
-        RunLogic();
-        DrawScreen();
-        LoopDelay();
-    }
-
-
-    CleanUp();
->>>>>>> 9326ca0dedc180c54a65b7726c964f6cd51bf2ab
 
 
 }
+
+
+
 
 void Initialize(void)
 {
    MacUILib_init();
    MacUILib_clearScreen();
 
-<<<<<<< HEAD
+
    //score = 0;
+
+
 
 
    myGM = new GameMechs(30,15); //new heap
    myPlayer = new Player(myGM); //new heap
    myFood = new Food(myGM); //new heap
 
+
    srand(time(NULL));
+
 
    objPos tempPos;
    myPlayer->getPlayerPos(tempPos);
    myFood->generateFood(tempPos);
 
+
    //think about when to generate the new food...
+
 
    // remember generateFood() requires player reference. You will need to provide
    // it after player object is instantiated
-=======
-
-    myGM = new GameMechs(26,16); //new heap
-    myPlayer = new Player(myGM); //new heap
->>>>>>> 9326ca0dedc180c54a65b7726c964f6cd51bf2ab
 }
+
+
 
 
 void GetInput(void)
 {
-<<<<<<< HEAD
   myGM -> getInput();
 }
+
+
+
+
 
 
 
@@ -151,19 +165,24 @@ void RunLogic(void)
    myPlayer->updatePlayerDir();
    myPlayer->movePlayer();
 
+
    
    
 //    objPos returnPlayerPos;
 //    myPlayer->getPlayerPos(returnPlayerPos)
 
+
 //    objPos FoodPos;
 //    myFood->getFoodPos(FoodPos);
+
 
    objPos tempPos;
    myPlayer->getPlayerPos(tempPos); //get the player pos.
 
+
    objPos foodPos;
    myFood->getFoodPos(foodPos);
+
 
    if(tempPos.isPosEqual(&foodPos))
    {
@@ -173,10 +192,15 @@ void RunLogic(void)
 
 
 
+
+
+
    
 //    objPos blockOff;
 //    myFood->generateFood(blockOff);
 //    //myFood->getFoodPos();
+
+
 
 
     myGM -> clearInput();
@@ -186,10 +210,18 @@ void RunLogic(void)
 
 
 
+
+
+
+
 void DrawScreen(void)
 {
-   MacUILib_clearScreen();   
+   MacUILib_clearScreen();  
    int y, x;
+
+
+
+
 
 
 
@@ -197,16 +229,20 @@ void DrawScreen(void)
    objPos tempPos;
    myPlayer->getPlayerPos(tempPos); //get the player pos.
 
+
    objPos foodPos;
    myFood->getFoodPos(foodPos);
    
-   MacUILib_printf("Controls: 'w' = Up,'a' = Left, 's' = Down, 'd' = Right\nPress ESC to exit game\n");
+   MacUILib_printf("Controls: Press 'w' for Up, Press 'a' for Left, Press 's' for Down, Press 'd' Right\nPress ESC to exit game\n");
+
 
    for(y = 0; y < myGM -> getBoardSizeY(); y++)
    {
        for(x = 0; x < myGM -> getBoardSizeX(); x++)
        {
            char printChar = ' ';
+
+
 
 
            if(y == 0 || x == 0 || x == myGM -> getBoardSizeX() - 1|| y == myGM -> getBoardSizeY() - 1)
@@ -227,58 +263,12 @@ void DrawScreen(void)
        MacUILib_printf("\n");
    }
 
+
    MacUILib_printf("BoardSize: %dx%d\nPlayer Pos: <%d, %d>\nCharacter: %c\nScore: %d\n",
                    myGM->getBoardSizeX(), myGM->getBoardSizeY(),
                    tempPos.x, tempPos.y, tempPos.symbol, myGM->getScore());
-=======
-   myGM -> getInput();
-}
 
 
-void RunLogic(void)
-{
-    myPlayer->updatePlayerDir();
-    myPlayer->movePlayer();
-
-    myGM -> clearInput();
-}
-
-
-void DrawScreen(void)
-{
-    MacUILib_clearScreen();    
- 
-    int y, x;
-
-
-    objPos tempPos;
-    myPlayer->getPlayerPos(tempPos); //get the player pos.
-
-
-   MacUILib_printf("BoardSize: %dx%d, Player Pos: <%d, %d>, Character: %c\n", 
-                    myGM->getBoardSizeX(), myGM->getBoardSizeY(),
-                    tempPos.x, tempPos.y, tempPos.symbol);
-
-    for(y = 0; y < myGM -> getBoardSizeY(); y++)
-    {
-        for(x = 0; x < myGM -> getBoardSizeX(); x++)
-        {
-            char printChar = ' ';
-
-            if(y == 0 || x == 0 || x == myGM -> getBoardSizeX() - 1|| y == myGM -> getBoardSizeY() - 1)
-            {
-                printChar = '#';
-            }
-            else if(y == tempPos.y && x == tempPos.x)
-            {
-               printChar = tempPos.symbol;
-               
-            }
-            MacUILib_printf("%c", printChar);
-        }
-        MacUILib_printf("\n");
-    }
->>>>>>> 9326ca0dedc180c54a65b7726c964f6cd51bf2ab
 
 
 }
@@ -289,11 +279,15 @@ void LoopDelay(void)
    MacUILib_Delay(DELAY_CONST); // 0.1s delay
 }
 
-<<<<<<< HEAD
+
 void CleanUp(void)
 {
-   MacUILib_clearScreen();   
+   MacUILib_clearScreen();  
    MacUILib_uninit();
+
+
+
+
 
 
 
@@ -301,20 +295,7 @@ void CleanUp(void)
    delete myGM;
    delete myPlayer;
    delete myFood;
-=======
-
-
-
-void CleanUp(void)
-{
-    MacUILib_clearScreen();    
- 
-    MacUILib_uninit();
-
-
-    delete myGM;
-    delete myPlayer;
->>>>>>> 9326ca0dedc180c54a65b7726c964f6cd51bf2ab
 }
+
 
 
