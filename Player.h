29a3178ea7556b73bd73e4feefbedc57ce28a2f3
wildingk1 <144181997+wildingk1@users.noreply.github.com>
2023-11-30 20:03:30 -0500

    11/30 8:02pm

--- Player.h
+++ Player.h
@@ -2,9 +2,13 @@
 #define PLAYER_H
 
 
+
+
 #include "GameMechs.h"
 #include "objPos.h"
 #include "objPosArrayList.h"
+
+
 
 
 class Player
@@ -12,18 +16,28 @@ class Player
    // Construct the remaining declaration from the project manual.
 
 
+
+
    // Only some sample members are included here
 
 
+
+
    // You will include more data members and member functions to complete your design.
+
+
 
 
    public:
        enum Dir {UP, DOWN, LEFT, RIGHT, STOP};  // This is the direction state
 
 
+
+
        Player(GameMechs* thisGMRef);
        ~Player();
+
+
 
 
        void getPlayerPos(objPos &returnPos); // Upgrade this in iteration 3.
@@ -31,9 +45,13 @@ class Player
        void movePlayer();
 
 
+
+
    private:
        objPos playerPos;   // Upgrade this in iteration 3.      
        enum Dir myDir;
+
+
 
 
        // Need a reference to the Main Game Mechanisms
@@ -41,5 +59,52 @@ class Player
 };
 
 
+
+
 #endif
+
+
+objPos.h 
+
+#ifndef OBJPOS_H
+#define OBJPOS_H
+
+
+
+
+class objPos
+{
+   public:
+       int x;
+       int y;
+       char symbol;
+
+
+
+
+       objPos();
+       objPos(objPos &o); // copy constructor
+       objPos(int xPos, int yPos, char sym);
+
+
+
+
+       void setObjPos(objPos o);      
+       void setObjPos(int xPos, int yPos, char sym);
+       void getObjPos(objPos &returnPos);
+       char getSymbol();
+
+
+
+
+       bool isPosEqual(const objPos* refPos);
+     
+       char getSymbolIfPosEqual(const objPos* refPos);
+};
+
+
+
+
+#endif
+
 
