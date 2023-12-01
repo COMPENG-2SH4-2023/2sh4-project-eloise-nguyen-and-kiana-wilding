7841f43c2433a67cf7a6cb46563bfa62c49ba011
wildingk1 <144181997+wildingk1@users.noreply.github.com>
2023-11-30 20:07:54 -0500

    11/30 8:02pm

--- GameMechs.h
+++ GameMechs.h
@@ -2,15 +2,25 @@
 #define GAMEMECHS_H
 
 
+
+
 #include <cstdlib>
 #include <time.h>
+
+
 
 
 #include "objPos.h"
 #include "objPosArrayList.h"
 
 
+
+
 using namespace std;
+
+
+
+
 
 
 
@@ -20,55 +30,45 @@ class GameMechs
    // Construct the remaining declaration from the project manual.
 
 
+
+
    // Only some sample members are included here
 
-    private:
-        char input;
-        bool exitFlag;
-        bool loseFlag;
-        int score;
-        
-        int boardSizeX;
-        int boardSizeY;
 
-    public:
-        GameMechs();
-        GameMechs(int boardX, int boardY);
-        ~GameMechs(); //Destructor 
-        
-        bool getExitFlagStatus();
-        bool getLoseFlagStatus();
 
-        void setExitTrue();
-        void setLoseFlag();
 
-        char getScore();
-        char getInput();
-        void setInput(char this_input);
-        void clearInput();
-        void incrementScore();
+   // You will include more data members and member functions to complete your design.
+
+
+
 
    private:
        char input;
        bool exitFlag;
        bool loseFlag;
        int score;
-      
+     
        int boardSizeX;
        int boardSizeY;
+
+
 
 
    public:
        GameMechs();
        GameMechs(int boardX, int boardY);
        ~GameMechs(); //Destructor
-      
+     
        bool getExitFlagStatus();
        bool getLoseFlagStatus();
 
 
+
+
        void setExitTrue();
        void setLoseFlag();
+
+
 
 
        char getScore();
@@ -78,12 +78,20 @@ class GameMechs
        void incrementScore();
 
 
+
+
        int getBoardSizeX();
        int getBoardSizeY();
-    
+   
+
+
 
 
 };
 
 
+
+
 #endif
+
+
